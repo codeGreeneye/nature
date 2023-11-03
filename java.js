7e45c6d9bbe48c833878282e8adce12151b2f2b8
codeGreeneye <149160134+codeGreeneye@users.noreply.github.com>
2023-11-03 14:33:50 +0000

    files

---
+++ java.js
@@ -0,0 +1,31 @@
+alert("INSECTS ALSO FEEL PAIN");
+
+const friend = "silkwarm" ;
+const x = "insect"
+const species = "caterpillars";
+const color = "white";
+const height = "2-3 cm";
+const statement = " is use for making silk ";
+const completeStatement = friend + statement;
+
+console.log(completeStatement)
+
+//silkwarm is the insects. They are caterpillars of white color. their height is  4–6 cm
+const sentence = friend + " is the insect. They are " + species + " of " + color +" colour. " + " They are about " + height + " long. "
+console.log(sentence);
+
+const z = 20;
+const y = 15;
+const result = z - y;
+console.log(result);
+
+const a = 20;
+const b = 10;
+const multi = a * b;
+console.log(multi);
+
+const p = 20;
+const q = 40;
+const  divide = q /  p;
+console.log(divide);
+
